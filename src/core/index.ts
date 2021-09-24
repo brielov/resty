99ef3c85fc3dadf3845c23806c53668d4a159d4c
Gabriel Vaquer <brielov@icloud.com>
2021-09-24 12:01:17 -0300

    Export types and interfaces using `export type`

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,8 @@
-export { Headers, ReadonlyHeaders, HeadersInit } from "./headers";
+export type { HeadersInit } from "./headers";
+export { Headers, ReadonlyHeaders } from "./headers";
 export { HttpError } from "./error";
 export { HttpStatus } from "./status";
 export { Request } from "./request";
-export { RequestHandler, resolve } from "./resolver";
+export type { RequestHandler } from "./resolver";
+export { resolve } from "./resolver";
 export { Response } from "./response";
